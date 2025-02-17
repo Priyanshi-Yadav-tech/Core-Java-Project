dabf10f6b559a1cf4b4c99203af569e5f900ca35
Priyanshi <yadavpriyanshi61@gmail.com>
2025-02-17 18:07:00 +0530

    second_commit

--- Demo/src/com/priyanshi/demo/Demo.java
+++ Demo/src/com/priyanshi/demo/Demo.java
@@ -3,5 +3,9 @@ package com.priyanshi.demo;
 public class Demo {
 	public static void main(String[] args) {
 		System.out.println("Hello World"); 
+		int a =10;
+		int b=20;
+		int sum =a+b;
+		System.out.println("sum of two "+sum);
 	}
 }
